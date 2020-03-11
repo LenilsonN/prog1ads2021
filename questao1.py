9e08f0218434f65b5f651111c01e440136f8b9cd
LenilsonN <lenilsonnobrega@ads.fiponline.com>
2020-03-11 20:43:38 -0300

    acfkbsfuidasbu

--- questao1.py
+++ questao1.py
@@ -1,4 +1,4 @@
 preco_gas = input("Insira o preço da gasolina: ")
 compra = input("Insira o quanto deseja gastar: ")
 total = float(compra)/float(preco_gas)
-print("O total de litros adquiridos é: {:.2f}".format(total))
+print("O total de litros adquiridos é de: {:.2f}".format(total))
