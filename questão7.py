3a2d6714680791178c5f64df34e67d0bce2d221f
LenilsonN <lenilsonnobrega@ads.fiponline.com>
2020-03-15 16:27:04 -0300

    Questão 7

---
+++ questão7.py
@@ -0,0 +1,13 @@
+largura1 = float(input("Largura do primeiro cômodo: "))
+comprimento1 = float(input("comprimento do primeiro cômodo: "))
+largura2 = float(input("Largura do segundo cômodo: "))
+comprimento2 = float(input("comprimento do segundo cômodo: "))
+largura3 = float(input("Largura do terceiro cômodo: "))
+comprimento3 = float(input("comprimento do terceiro cômodo: "))
+largura4 = float(input("Largura do quarto cômodo: "))
+comprimento4 = float(input("comprimento do quarto cômodo: "))
+area1 = largura1 * comprimento1
+area2 = largura2 * comprimento2
+area3 = largura3 * comprimento3
+area4 = largura4 * comprimento4
+print("Comodo 1: ",area1, "\nComodo 2: ",area2,"\nComodo 3: ",area3,"\nComodo 4: ",area4,"\nArea Total: ", (area1+area2+area3+area4))
